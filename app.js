e3e198575b063d9ede60db3fed5ef2666fb4f5ce
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-07-07 21:31:06 +0200

    Hidden class skal tilpasses, så de tre muligheder ikke vises

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 window.addEventListener("load", start);
 
-
 function start() {
-    console.log("JS kører");
+  console.log("JS kører");
+  document.querySelector("#rock-button").addEventListener("click", chooseRock);
+  document.querySelector("#paper-button").addEventListener("click", choosePaper);
+  document.querySelector("#scissors-button").addEventListener("click", chooseScissors);
+}
+
+function chooseRock() {
+    console.log("Rock chosen");
+    document.querySelector
+}
+
+function choosePaper() {
+  console.log("Paper chosen");
+}
+
+function chooseScissors() {
+  console.log("Scissors chosen");
 }
